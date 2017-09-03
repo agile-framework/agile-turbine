f52a35584536c74e82680dbe5e2581fe5b2fdb6c
mydeathtrial <mydeathtrial@163.com>
2017-09-03 23:01:22 +0800

    v1.0

---
+++ src/main/java/com/agileframework/hystrixturbine/AgileTurbineApplication.java
@@ -0,0 +1,15 @@
+package com.agileframework.hystrixturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+//开启turbine统计
+@EnableTurbine
+public class AgileTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileTurbineApplication.class, args);
+	}
+}
